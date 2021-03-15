2c137b71c718b4d3d666eae18f20728efba47f0e
Ewa-Gr <77926514+Ewa-Gr@users.noreply.github.com>
2021-03-15 21:16:12 +0100

    Current Location Forecast update

--- src/app.js
+++ src/app.js
@@ -207,6 +207,9 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTempAndCity);
 
+  apiUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showForecast);
+
 }
 
 function getCurrentLocation(event) {
